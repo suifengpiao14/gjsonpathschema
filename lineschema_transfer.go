aecbf725453a01bc9671b3815c9deccaf4c0f8c5
zheng peng <pengzheng@huishoubao.com.cn>
2023-12-01 09:12:07 +0800

    optimize

--- lineschema_transfer.go
+++ lineschema_transfer.go
@@ -3,6 +3,7 @@ package lineschema
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"strings"
@@ -379,22 +380,47 @@ func str2StructTransfer(rt reflect.Type, prefix string) (transfers Transfers) {
 	return transfers
 }
 
-// TransferPackHandler 转换数据stream 生成函数
-func TransferPackHandler(beforGjsonPath string, afterGjsonPath string) (packHandler stream.PackHandler) {
-	packHandler = stream.NewPackHandler(func(ctx context.Context, input []byte) (out []byte, err error) {
-		if beforGjsonPath == "" {
-			return input, nil
-		}
-		str := gjson.GetBytes(input, beforGjsonPath).String()
-		out = []byte(str)
-		return out, nil
-	}, func(ctx context.Context, input []byte) (out []byte, err error) {
-		if afterGjsonPath == "" {
-			return input, nil
-		}
-		str := gjson.GetBytes(input, afterGjsonPath).String()
-		out = []byte(str)
-		return out, nil
-	})
-	return packHandler
+type _TransferPacketHandler struct {
+	BeforGjsonPath string `json:"beforGjsonPath"`
+	AfterGjsonPath string `json:"afterGjsonPath"`
+}
+
+//NewTransferPacketHandler json转换
+func NewTransferPacketHandler(beforGjsonPath string, afterGjsonPath string) (packet stream.PacketHandlerI) {
+	return &_TransferPacketHandler{
+		BeforGjsonPath: beforGjsonPath,
+		AfterGjsonPath: afterGjsonPath,
+	}
+}
+
+func (packet *_TransferPacketHandler) Name() string {
+	return stream.GeneratePacketHandlerName(packet)
+}
+
+func (packet *_TransferPacketHandler) Description() string {
+	return `将json数据进行变换,支持类型和路径变换`
+}
+
+func (packet *_TransferPacketHandler) String() string {
+	b, _ := json.Marshal(packet)
+	s := string(b)
+	return s
+}
+
+func (packet *_TransferPacketHandler) Before(ctx context.Context, input []byte) (newCtx context.Context, out []byte, err error) {
+	if packet.BeforGjsonPath == "" {
+		return ctx, input, nil
+	}
+	str := gjson.GetBytes(input, packet.BeforGjsonPath).String()
+	out = []byte(str)
+	return ctx, out, nil
+}
+
+func (packet *_TransferPacketHandler) After(ctx context.Context, input []byte) (newCtx context.Context, out []byte, err error) {
+	if packet.AfterGjsonPath == "" {
+		return ctx, input, nil
+	}
+	str := gjson.GetBytes(input, packet.AfterGjsonPath).String()
+	out = []byte(str)
+	return ctx, out, nil
 }
