9cf4d311ef78ee6dca3326664de1e3fe49c1dad1
zheng peng <pengzheng@huishoubao.com.cn>
2023-12-01 13:34:17 +0800

    upgrade

--- go.mod
+++ go.mod
@@ -6,10 +6,9 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cast v1.5.1
 	github.com/stretchr/testify v1.8.4
-	github.com/suifengpiao14/funcs v0.0.16
+	github.com/suifengpiao14/funcs v0.0.17
 	github.com/suifengpiao14/gjsonmodifier v0.1.9
 	github.com/suifengpiao14/kvstruct v0.0.14
-	github.com/suifengpiao14/stream v0.0.16
 	github.com/tidwall/gjson v1.17.0
 	github.com/tidwall/sjson v1.2.5
 )
@@ -17,12 +16,7 @@ require (
 require (
 	github.com/d5/tengo/v2 v2.13.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator v9.31.0+incompatible // indirect
-	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/suifengpiao14/logchan/v2 v2.0.22 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- lineschema_transfer.go
+++ lineschema_transfer.go
@@ -2,16 +2,12 @@ package lineschema
 
 import (
 	"bytes"
-	"context"
-	"encoding/json"
 	"fmt"
 	"reflect"
 	"strings"
 
 	"github.com/spf13/cast"
 	"github.com/suifengpiao14/funcs"
-	"github.com/suifengpiao14/stream"
-	"github.com/tidwall/gjson"
 )
 
 type TransferUnit struct {
@@ -378,49 +374,4 @@ func str2StructTransfer(rt reflect.Type, prefix string) (transfers Transfers) {
 	}
 
 	return transfers
-}
-
-type _TransferPacketHandler struct {
-	BeforGjsonPath string `json:"beforGjsonPath"`
-	AfterGjsonPath string `json:"afterGjsonPath"`
-}
-
-//NewTransferPacketHandler json转换
-func NewTransferPacketHandler(beforGjsonPath string, afterGjsonPath string) (packet stream.PacketHandlerI) {
-	return &_TransferPacketHandler{
-		BeforGjsonPath: beforGjsonPath,
-		AfterGjsonPath: afterGjsonPath,
-	}
-}
-
-func (packet *_TransferPacketHandler) Name() string {
-	return stream.GeneratePacketHandlerName(packet)
-}
-
-func (packet *_TransferPacketHandler) Description() string {
-	return `将json数据进行变换,支持类型和路径变换`
-}
-
-func (packet *_TransferPacketHandler) String() string {
-	b, _ := json.Marshal(packet)
-	s := string(b)
-	return s
-}
-
-func (packet *_TransferPacketHandler) Before(ctx context.Context, input []byte) (newCtx context.Context, out []byte, err error) {
-	if packet.BeforGjsonPath == "" {
-		return ctx, input, nil
-	}
-	str := gjson.GetBytes(input, packet.BeforGjsonPath).String()
-	out = []byte(str)
-	return ctx, out, nil
-}
-
-func (packet *_TransferPacketHandler) After(ctx context.Context, input []byte) (newCtx context.Context, out []byte, err error) {
-	if packet.AfterGjsonPath == "" {
-		return ctx, input, nil
-	}
-	str := gjson.GetBytes(input, packet.AfterGjsonPath).String()
-	out = []byte(str)
-	return ctx, out, nil
 }
